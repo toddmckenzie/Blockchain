cb9076cdb22aeb379b3b7b8b385c2e791cebf715
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-12-05 16:57:25 -0600

    create react app set up

---
+++ ReactApp/wallet/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [ id, setId ] = useState('');
+  const [ newId, setNewId ] = useState('');
+  const [ currentBalance, setCurrentBalance ] = useState('');
+  const [ transactions, setTransactions ] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('localhost:4006/mine')
+      .then(res => {
+        console.log(res.data)
+        setCurrentBalance(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },[])
+
+  useEffect(() => {
+    axios.get('localhost:4006/chain')
+      .then(res => {
+        setTransactions(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+
+  changeId = (id)
+    axios.post(`localhost:4006/${id}/${newId}`)
+      .then(res => {
+        console.log(res.data)
+        setId(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  return (
+    <div className="App">
+      <h1 style={{textAlign: "center"}}>Wallet</h1>
+      <input placeholder="Enter id" onChange={(e) => e.target.value} />
+      <button onClick={() => enterId()}>Enter id</button>
+      <h4 style={{textAlign: "center"}}>Balance: {currentBalance}</h4>
+      <
+    </div>
+  );
+}
+
+export default App;
